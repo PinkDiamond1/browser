7f31284c0b246a011c7f07b0317395cb77eeba2c
Musiczombie <j18668179275@163.com>
2019-12-09 16:54:11 +0800

    add parent signer

--- db/action.go
+++ db/action.go
@@ -12,34 +12,36 @@ import (
 )
 
 type MysqlAction struct {
-	TxHash          string
-	ActionHash      string
-	ActionIndex     int
-	Nonce           uint64
-	Height          uint64
-	Created         uint64
-	GasAssetId      uint64
-	TransferAssetId uint64
-	ActionType      uint64
-	From            string
-	To              string
-	Amount          *big.Int
-	GasLimit        uint64
-	GasUsed         uint64
-	State           uint64
-	ErrorMsg        string
-	Remark          []byte
-	Payload         []byte
-	PayloadSize     int
-	InternalCount   int
-	Payer           string
-	PayerGasPrice   *big.Int
+	TxHash            string
+	ActionHash        string
+	ActionIndex       int
+	Nonce             uint64
+	Height            uint64
+	Created           uint64
+	GasAssetId        uint64
+	TransferAssetId   uint64
+	ActionType        uint64
+	From              string
+	To                string
+	Amount            *big.Int
+	GasLimit          uint64
+	GasUsed           uint64
+	State             uint64
+	ErrorMsg          string
+	Remark            []byte
+	Payload           []byte
+	PayloadSize       int
+	InternalCount     int
+	Payer             string
+	PayerGasPrice     *big.Int
+	ParentSigner      string
+	PayerParentSigner string
 }
 
 func InsertAction(data *MysqlAction, dbTx *sql.Tx) error {
 	tName := GetTableNameHash("actions_hash", data.TxHash)
 	insertSql := fmt.Sprintf("insert into %s (tx_hash, action_hash, action_index,nonce,height,created,gas_asset_id,transfer_asset_id,"+
-		"action_type,from_account,to_account,amount,gas_limit,gas_used,state,error_msg,remark,payload,payload_size,internal_action_count, payer, payer_gas_price) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", tName)
+		"action_type,from_account,to_account,amount,gas_limit,gas_used,state,error_msg,remark,payload,payload_size,internal_action_count, payer, payer_gas_price, parent_signer, payer_parent_signer) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", tName)
 	stmt, err := dbTx.Prepare(insertSql)
 	defer stmt.Close()
 	if err != nil {
@@ -47,7 +49,7 @@ func InsertAction(data *MysqlAction, dbTx *sql.Tx) error {
 	}
 	state := strconv.FormatUint(data.State, 10)
 	_, err = stmt.Exec(data.TxHash, data.ActionHash, data.ActionIndex, data.Nonce, data.Height, data.Created, data.GasAssetId, data.TransferAssetId,
-		data.ActionType, data.From, data.To, data.Amount.String(), data.GasLimit, data.GasUsed, state, data.ErrorMsg, data.Remark, data.Payload, data.PayloadSize, data.InternalCount, data.Payer, data.PayerGasPrice.String())
+		data.ActionType, data.From, data.To, data.Amount.String(), data.GasLimit, data.GasUsed, state, data.ErrorMsg, data.Remark, data.Payload, data.PayloadSize, data.InternalCount, data.Payer, data.PayerGasPrice.String(), data.ParentSigner, data.PayerParentSigner)
 	if err != nil {
 		ZapLog.Panic("InsertAction error", zap.Error(err), zap.String("txHash", data.TxHash))
 	}
